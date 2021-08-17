91b12441f4b117c52b06e8e9e14739ac16363378
Daro Webdeveloper <darowebdeveloper@gmail.com>
2021-08-17 21:48:33 +0700

    build docs for page

---
+++ docs/index.9b563e39.js
@@ -0,0 +1,2 @@
+document.querySelector(".video__play-button").addEventListener("click",(e=>{e.preventDefault(),e.target.style.display="none",document.querySelector(".video__content").play()}));
+//# sourceMappingURL=index.9b563e39.js.map
